b80a980bf326abfacd6e874c5697f6f8dc87daae
Elahe <elahesalari.edu@yahoo.com>
2022-10-28 16:00:36 +0330

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	.idea/.gitignore
    #	.idea/misc.xml
    #	.idea/modules.xml
    
    Part4 is update

--- homeWorkOne/Part 4.py
+++ homeWorkOne/Part 4.py
@@ -4,11 +4,12 @@ import imutils
 import os
 import matplotlib.pyplot as plt
 
-
 def process_image():
     path = 'data'
-    for filename in os.listdir(path):
+    filenames = ['1.jpg', '2.jpg', '3.jpg']
+    for filename in filenames:
         image = cv2.imread(os.path.join(path, filename))
+
         gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
 
         ddepth = cv2.cv.CV_32F if imutils.is_cv2() else cv2.CV_32F
@@ -18,18 +19,18 @@ def process_image():
         gradient = cv2.subtract(gradX, gradY)
         gradient = cv2.convertScaleAbs(gradient)
 
-        # blurred = cv2.blur(gradient, (15, 15))
-        blurred = cv2.bilateralFilter(edge_enh, 13, 50, 50)
+        blurred = cv2.blur(gradient, (27, 27))
+        # blurred = cv2.bilateralFilter(edge_enh, 13, 50, 50)
         (_, thresh) = cv2.threshold(blurred, 200, 255, cv2.THRESH_BINARY)
 
-        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (19, 19))
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (25, 25))
         closed = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
 
         plt.imshow(closed, cmap='gray')
         plt.show()
 
+        closed = cv2.dilate(closed, None, iterations=8)
         closed = cv2.erode(closed, None, iterations=4)
-        closed = cv2.dilate(closed, None, iterations=4)
 
         plt.imshow(closed, cmap='gray')
         plt.show()
